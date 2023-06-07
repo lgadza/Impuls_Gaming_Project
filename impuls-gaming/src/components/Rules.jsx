f05067c8a62d31f0c4d554e92a6094f7cbad0cd5
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-06-07 10:28:37 +0200

    rules feature

--- impuls-gaming/src/components/Rules.jsx
+++ impuls-gaming/src/components/Rules.jsx
@@ -15,6 +15,7 @@ const Rules = () => {
             <div className="d-flex  flex-column my-3">
               <h4 className="d-flex">Rules</h4>
               {<span className="d-flex">{tournament.rules}</span>}
+              {/* <span className="d-flex">Rules not available.</span> */}
             </div>
           </Col>
           <Col></Col>
